622760b3aa5bc7f42d25cb8add902263de75364c
minsol <ohimsoly@gmail.com>
2023-03-15 20:49:19 +0900

    게임 화면에 TEXT

--- first-unity/Assets/scripts/Test2.cs
+++ first-unity/Assets/scripts/Test2.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test2 : MonoBehaviour
 {
+    // Declare member variables
     public int hp = 180;
+    public Text textHpUi;
+    public Text textStateUi;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,28 +19,37 @@ public class Test2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /* ==========================================================
+         * UI
+         * ========================================================== */
+        textHpUi.text = hp.ToString();
 
-        if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼 클릭
+        if (hp <= 50)
+        {
+           textStateUi.text ="Run!";
+        }
+        else if (hp >= 200)
+        {
+            textStateUi.text = "Attack!";
+        }
+        else
+        {
+            textStateUi.text = "Defense!";
+        }
+
+        /* ==========================================================
+         * Input
+         * ========================================================== */
+
+
+        if (Input.GetMouseButtonDown(0)) // Click mouse left button
         {
             hp += 10;
         }
 
-        if (Input.GetMouseButtonDown(1)) // 마우스 오른쪽 버튼 클릭
+        if (Input.GetMouseButtonDown(1)) // Click mouse right button
         {
             hp -= 10;
-        }
-
-        if (hp <= 50)
-        {
-            Debug.Log("Run!");
-        }
-        else if (hp >= 200)
-        {
-            Debug.Log("Attack!");
-        }
-        else
-        {
-            Debug.Log("Defense!");
         }
     }
 }
