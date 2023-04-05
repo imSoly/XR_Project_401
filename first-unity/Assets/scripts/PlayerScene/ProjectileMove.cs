418a27b31032d16dc7ba20d6ddf44668ef01e651
minsol <ohimsoly@gmail.com>
2023-04-05 20:56:02 +0900

    플레이어피봇 생성부터 몬스터 데미지 입혀 없애기까지

---
+++ first-unity/Assets/scripts/PlayerScene/ProjectileMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    public Vector3 launchDirection; // 발사 방향
+
+    private void FixedUpdate()
+    {
+        float moveAmount = 10 * Time.fixedDeltaTime; // 이동 속도 설정
+        transform.Translate(launchDirection * moveAmount); // Translate로 이동
+    }
+
+    private void OnCollisionEnter(Collision collision) // 충돌이 일어났을 경우
+    {
+        Debug.Log(collision.gameObject.name);
+
+        // Tag 값이 Object인 경우
+        if (collision.gameObject.tag == "Object") 
+        {
+            Destroy(this.gameObject); // 발사체 삭제
+        }
+        // Tag 값이 Monster인 경우
+        if (collision.gameObject.tag == "Monster")
+        {
+            Destroy(this.gameObject); // 발사체 삭제
+            collision.gameObject.GetComponent<Monster>().Damaged(1);
+        }
+    }
+}
